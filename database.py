ee7ae3bab5e90f8f4fbd44a4d9ae203748585f62
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-04-05 14:05:00 +0100

    feat: configure SQLAlchemy ORM and SQLite database connectivity

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+"""
+the line below holds the location of the database in the app directory 
+"""
+SQLALCHEMY_DATABASE_URI = 'sqlite:///./todos.db'
+
+
+"""
+The line below creates the link between python code and the database
+software
+"""
+engine =  create_engine(SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
