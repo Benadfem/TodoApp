ff9e2e847d3fdd0669992d7c6cb9b2067f038231
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-04-05 23:16:43 +0100

    feat: implement dependency injection and root GET endpoint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from typing import Annotated
+
 import models
-from database import engine
+from database import engine, SessionLocal
 
 app = FastAPI()
 
@@ -10,3 +13,16 @@ since there is model an database connection creation
 """
 models.Base.metadata.create_all(engine)
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# let's create a variable db_dependency to hold the annotation
+db_dependency =Annotated[Session,Depends(get_db)]
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(models.Todos).all()
