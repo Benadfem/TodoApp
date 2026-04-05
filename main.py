5a814b518d5b2c557e82490df215b7ffff7788aa
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-04-05 22:07:13 +0100

    feat: complete database integration and todos model definition

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+import models
+from database import engine
 
 app = FastAPI()
+
+"""
+fastapi will automatically create a database for the applicaiton 
+since there is model an database connection creation 
+"""
+models.Base.metadata.create_all(engine)
+
