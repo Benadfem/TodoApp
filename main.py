956a3b0c3d215afb841574d53b15782155ae7421
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-04-08 13:38:44 +0100

    feat: implement DELETE endpoint and complete CRUD lifecycle

--- main.py
+++ main.py
@@ -103,3 +103,15 @@ async def read_todo_by_priority(db: db_dependency,
         return todo_model
 
     raise HTTPException(status_code=404, detail='No todos found with that priority.')
+
+"""
+We can decide to delete some data from the data base by todo_id
+"""
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency,todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found.")
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+
+    db.commit()
