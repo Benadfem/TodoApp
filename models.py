6086338bf5091151de1606699e69862b7a594801
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-04-10 01:23:56 +0100

    feat(models): establish One-to-Many relationship between Users and Todos

--- models.py
+++ models.py
@@ -1,5 +1,19 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+"""
+let's create a class for the users model this is also called the schema
+"""
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    roles = Column(String)
 
 
 """
@@ -12,3 +26,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
