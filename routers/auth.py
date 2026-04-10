1030b5b95e2191df31be6188421276d7f4a6d116
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-04-10 02:54:56 +0100

    feat(auth): implement secure password hashing using passlib CryptContext

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
+from passlib.context import CryptContext
+from passlib.handlers.bcrypt import bcrypt
 from pydantic import BaseModel
 
 from models import User
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 class CreateUserRequest(BaseModel):
     email: str
     password: str
@@ -18,7 +23,7 @@ async def create_user(create_user_request: CreateUserRequest):
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         roles=create_user_request.roles,
-        hashed_password = create_user_request.password,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
     return create_user_model
