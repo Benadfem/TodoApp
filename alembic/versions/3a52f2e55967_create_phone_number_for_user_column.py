77fb41bcd00cf25b157d9aa710e764cd8daf595b
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-05-05 16:20:40 +0100

    Created an additional column in the users database through Alembic Revisio

---
+++ alembic/versions/3a52f2e55967_create_phone_number_for_user_column.py
@@ -0,0 +1,28 @@
+"""create phone number for user column
+
+Revision ID: 3a52f2e55967
+Revises: 
+Create Date: 2026-05-05 16:02:04.516759
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '3a52f2e55967'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column('users', sa.Column('phone_number', sa.String(), nullable=True))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
